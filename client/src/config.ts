80518554bb53891f99e04be8192553001faa5a39
nevin.e <nevin.e@inapp.com>
2024-09-11 18:29:19 +0530

    updated config

--- client/src/config.ts
+++ client/src/config.ts
@@ -9,20 +9,20 @@ const awsVal = {
         apiGateWay: {
             NAME: "apiGateway",
             REGION: "ap-south-1",
-            URL: "https://gzjh5p6fk9.execute-api.ap-south-1.amazonaws.com/local",
+            URL: "https://g33b3lopr3.execute-api.ap-south-1.amazonaws.com/local",
         },
         cognito: {
             REGION: "ap-south-1",
-            USER_POOL_ID: "ap-south-1_6Z2RYSDak",
-            APP_CLIENT_ID: "2mis557ujhaas99769q5v9acf",
+            USER_POOL_ID: "ap-south-1_Ob7hz5roa",
+            APP_CLIENT_ID: "7qjq2hves89ck5dii9eucffdkn",
             DOMAIN: "ttapp-website-local.auth.ap-south-1.amazoncognito.com",
             SIGN_IN_URL: [
                 "http://localhost:5173/",
-                "https://dwdwwt4h4m5x8.cloudfront.net/index.html"
+                "https://d2xe55jtm2vl50.cloudfront.net/index.html"
             ],
             SIGN_OUT_URL: [
                 "http://localhost:5173/",
-                "https://dwdwwt4h4m5x8.cloudfront.net/index.html"
+                "https://d2xe55jtm2vl50.cloudfront.net/index.html"
             ],
         }
     },
@@ -38,8 +38,8 @@ const awsVal = {
             APP_CLIENT_ID: "3m7l1as5l4c2ulijjr0darcs95",
             IDENTITY_POOL_ID: "",
             DOMAIN: "ttapp-website-dev.auth.ap-south-1.amazoncognito.com",
-            SIGN_IN_URL: [" https://api-khel.ns-info.xyz/index.html"],
-            SIGN_OUT_URL: [" https://api-khel.ns-info.xyz/index.html"],
+            SIGN_IN_URL: ["https://api-khel.ns-info.xyz/index.html"],
+            SIGN_OUT_URL: ["https://api-khel.ns-info.xyz/index.html"],
         },
     },
     dev: {
@@ -54,8 +54,8 @@ const awsVal = {
             APP_CLIENT_ID: "3m7l1as5l4c2ulijjr0darcs95",
             IDENTITY_POOL_ID: "",
             DOMAIN: "ttapp-website-dev.auth.ap-south-1.amazoncognito.com",
-            SIGN_IN_URL: [" https://dev-khel.inapp.com/index.html"],
-            SIGN_OUT_URL: [" https://dev-khel.inapp.com/index.html"],
+            SIGN_IN_URL: ["https://dev-khel.inapp.com/index.html"],
+            SIGN_OUT_URL: ["https://dev-khel.inapp.com/index.html"],
         },
     },
     prod: {

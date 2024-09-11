848e0cdc069d5e37d1248c007235f56a9f40479c
nevin.e <nevin.e@inapp.com>
2024-09-11 20:26:50 +0530

    updated frontend

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,83 +1,22 @@
-
-type ENVType = "prod" | "local" | "dev" | "debug"
-
-const { VITE_ENV } = import.meta.env;
-
-
+// prod
 const awsVal = {
-    local: {
-        apiGateWay: {
-            NAME: "apiGateway",
-            REGION: "ap-south-1",
-            URL: "https://gzjh5p6fk9.execute-api.ap-south-1.amazonaws.com/local",
-        },
-        cognito: {
-            REGION: "ap-south-1",
-            USER_POOL_ID: "ap-south-1_6Z2RYSDak",
-            APP_CLIENT_ID: "2mis557ujhaas99769q5v9acf",
-            DOMAIN: "khelapp-website-local.auth.ap-south-1.amazoncognito.com",
-            SIGN_IN_URL: [
-                "http://localhost:5173/",
-                "https://dwdwwt4h4m5x8.cloudfront.net/index.html"
-            ],
-            SIGN_OUT_URL: [
-                "http://localhost:5173/",
-                "https://dwdwwt4h4m5x8.cloudfront.net/index.html"
-            ],
-        }
+    apiGateWay: {
+        NAME: "apiGateway",
+        REGION: "ap-south-1",
+        URL: "<APIGATEWAY_URL>",
     },
-    debug: {
-        apiGateWay: {
-            NAME: "apiGateway",
-            REGION: "ap-south-1",
-            URL: "https://api-khel.ns-info.xyz",
-        },
-        cognito: {
-            REGION: "ap-south-1",
-            USER_POOL_ID: "ap-south-1_HumibfFVT",
-            APP_CLIENT_ID: "3m7l1as5l4c2ulijjr0darcs95",
-            IDENTITY_POOL_ID: "",
-            DOMAIN: "khelapp-website-dev.auth.ap-south-1.amazoncognito.com",
-            SIGN_IN_URL: [" https://api-khel.ns-info.xyz/index.html"],
-            SIGN_OUT_URL: [" https://api-khel.ns-info.xyz/index.html"],
-        },
-    },
-    dev: {
-        apiGateWay: {
-            NAME: "apiGateway",
-            REGION: "ap-south-1",
-            URL: "https://api-dev-khel.inapp.com",
-        },
-        cognito: {
-            REGION: "ap-south-1",
-            USER_POOL_ID: "ap-south-1_HumibfFVT",
-            APP_CLIENT_ID: "3m7l1as5l4c2ulijjr0darcs95",
-            IDENTITY_POOL_ID: "",
-            DOMAIN: "khelapp-website-dev.auth.ap-south-1.amazoncognito.com",
-            SIGN_IN_URL: [" https://dev-khel.inapp.com/index.html"],
-            SIGN_OUT_URL: [" https://dev-khel.inapp.com/index.html"],
-        },
-    },
-    prod: {
-        apiGateWay: {
-            NAME: "apiGateWay Production",
-            REGION: "ap-south-1",
-            URL: "https://api-khel.inapp.com",
-        },
-        cognito: {
-            REGION: "ap-south-1",
-            USER_POOL_ID: "ap-south-1_kj1aiCw7X",
-            APP_CLIENT_ID: "7q558oguhbilfm1434tmb7noht",
-            IDENTITY_POOL_ID: "",
-            DOMAIN: "khelapp-website-prod.auth.ap-south-1.amazoncognito.com",
-            SIGN_IN_URL: ["https://khel.inapp.com/index.html"],
-            SIGN_OUT_URL: ["https://khel.inapp.com/index.html"],
-        },
-    },
-};
+    cognito: {
+        REGION: "ap-south-1",
+        USER_POOL_ID: "<USERPOOL_ID>",
+        APP_CLIENT_ID: "<USERPOOLCLIENT_ID>",
+        DOMAIN: "<DOMAIN_NAME>",
+        SIGN_IN_URL: ["<SIGNIN_URL>"],
+        SIGN_OUT_URL: ["<SIGNOUT_URL>"],
+    }
+}
 
 const GetAWSConfig = () => {
-    return awsVal[VITE_ENV as ENVType];
+    return awsVal;
 };
 
 export default GetAWSConfig;

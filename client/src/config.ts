cac398b502428a6659dc744b54b66f0feb0c1b97
nevin.e <nevin.e@inapp.com>
2024-09-13 23:12:23 +0530

    updated debug env

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,15 +3,15 @@ const awsVal = {
     apiGateWay: {
         NAME: "apiGateway",
         REGION: "ap-south-1",
-        URL: "https://api-khel.ns-info.xyz",
+        URL: "https://api-khel.ns-info.xyz/debug",
     },
     cognito: {
         REGION: "ap-south-1",
         USER_POOL_ID: "ap-south-1_BEYEfBa7Z",
         APP_CLIENT_ID: "53p555ctgca8uomqst3dgk89bj",
         DOMAIN: "khelapp-website-debug.auth.ap-south-1.amazoncognito.com",
-        SIGN_IN_URL: ["https://khel.ns-info.xyz/index.html"],
-        SIGN_OUT_URL: ["https://khel.ns-info.xyz/index.html"],
+        SIGN_IN_URL: ["https://khel.ns-info.xyz/"],
+        SIGN_OUT_URL: ["https://khel.ns-info.xyz/"],
     }
 }
 

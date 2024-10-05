f845f8ca13d393aab2a446a9f747f6b890ba98ee
nevin.e <nevin.e@inapp.com>
2024-10-05 19:08:00 +0530

    updaetd socket condition

--- api/functionStack/socket/index.mjs
+++ api/functionStack/socket/index.mjs
@@ -154,7 +154,7 @@ export async function main(event) {
             console.log(`oldImg: ${JSON.stringify(oldImg)}`);
 
 
-            if (newImg?.matchStatus === "LIVE" || newImg?.matchStatus === "FINISHED" && newImg?.role === "MATCH") {
+            if (newImg?.role === "MATCH" && (newImg?.matchStatus === "LIVE" || newImg?.matchStatus === "FINISHED")) {
 
                 const match = newImg;
 

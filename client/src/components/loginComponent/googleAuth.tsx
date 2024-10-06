960e62d413c91aa948eb3ca0e705b42cc684027a
nevin.e <nevin.e@inapp.com>
2024-10-07 00:29:53 +0530

    updated spinner

--- client/src/components/loginComponent/googleAuth.tsx
+++ client/src/components/loginComponent/googleAuth.tsx
@@ -35,19 +35,30 @@ const GoogleLoginButton: React.FC<GoogleLoginButtonTypeProp> = ({ isLoading }) =
           flex justify-center items-center font-medium rounded-lg py-3 px-4 w-full  
           text-lg
           ${isLoading
-            ? `bg-primary text-white transition-all duration-100 ease-custom cursor-default` 
-            : `bg-primary text-white transition-all duration-100 ease-custom
+              ? `bg-primary text-white transition-all duration-100 ease-custom cursor-default`
+              : `bg-primary text-white transition-all duration-100 ease-custom
                hover:border-[1px] hover:border-white hover:shadow-md`
-          }
+            }
         `}
           disabled={isLoading}
         >
-          <FontAwesomeIcon icon={faGoogle} className="mr-2 text-xl" />
-          Sign in with Google
-        </button>
+          {
+            isLoading ?
+              <>
+                {/* Spinner */}
+                <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-4 border-white mr-2"></div>
+                <span>Signing in...</span>
+              </>
+              :
+              <>
+                <FontAwesomeIcon icon={faGoogle} className="mr-2 text-xl" />
+                Sign in with Google
+              </>
+          }
+        </button >
         {signInError && <div className={`text-[14px] text-primary`}>{signInError}</div>}
 
-      </div>
+      </div >
     </>
   );
 };
